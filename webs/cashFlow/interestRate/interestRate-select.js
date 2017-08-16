efe3b2be257a5ca043844cc96a0bee1a2610337c
蒋彦颖 <jyy921221@gmail.com>
2017-08-16 15:30:07 +0800

    个人活期存款分段利率设置

---
+++ webs/cashFlow/interestRate/interestRate-select.js
@@ -0,0 +1,31 @@
+function initSelect(){
+	var selUrl="rate.json"
+	
+	$("#stateSel").select2({});
+	
+	var lineSel=$('#stateSel');
+	$.getJSON(selUrl,function(res){
+		res[0].state.forEach(function(val,index){
+			lineSel.append("<option>"+val+"</option>")
+		})
+	});
+	
+}
+
+//function initSelect() {
+//
+//	//define url for select data
+//	var selUrl = "rate.json"
+//	
+//	//init select
+//	$("#stateSel").select2({});
+//
+//	//init options to "#lineSel"
+//	var lineSel = $('#stateSel');
+//	$.getJSON(selUrl, function(res) {
+//		res[0].state.forEach(function(val, index) {
+//			lineSel.append("<option>" + val + "</option>")
+//		})
+//	});
+//
+//}
---
+++ webs/cashFlow/interestRate/interestRateIndex-table.js
@@ -0,0 +1,60 @@
+var columns = [{
+		field: 'termId',
+		title: '编号',
+		align: 'center'
+	}, {
+		field: 'termName',
+		title: '区间(左开右闭)',
+		align: 'center'
+	}, {
+		field: 'interestRate',
+		title: '利率(%)',
+		align: 'center'
+	}, {
+		field: 'termeFfective',
+		title: '生效日期',
+		align: 'center'
+	}, {
+		field: 'termInvalid',
+		title: '失效日期',
+		align: 'center'
+	}, {
+		field: 'state',
+		title: '状态',
+		align: 'center'
+	}, {
+		field: 'option',
+		title: '操作',
+		width: '80px',
+		align: 'center',
+		formatter: function(value, row, index) {
+			// var e = '<a href="#" class="btn btn-default" onclick="onSearch(\'' + index + '\')"><i class="fa fa-search" title="查看"></i></a> ';
+			//      var d = '<a href="#" class="btn btn-default" onclick=""><i class="glyphicon glyphicon-edit" title="编辑"></i></a> ';
+			var f = '<a href="#" class="btn btn-default" onclick=""><i class="glyphicon glyphicon-trash" title="失效"></i></a> ';
+			return f;
+		}
+
+}];
+function initTable(){
+	$('#table').bootstrapTable('destroy');
+	$('#table').bootstrapTable({
+		url:'interestRate.json',
+		cache:false,
+		pagination:true,
+		striped:true,
+		pageList:[10],
+		columns:columns
+	});
+}
+
+//function initTable() {
+//	$('#table').bootstrapTable('destroy');
+//  $('#table').bootstrapTable({
+//      url: 'interestRate.json',
+//      cache: false, //是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
+//      pagination: true, //是否显示分页（*）
+//      striped: true,
+//      pageList:[10],
+//      columns: columns	//列配置项
+//  });
+//}
